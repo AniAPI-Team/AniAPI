27f3f1bc0b4f0b731148ced355bb25565c1f4e2a
Emerson Annoni <emerson.annoni@openjob.it>
2021-01-24 18:16:08 +0100

    Added Theme Manager - Program.cs

--- AniApi/Client/Program.cs
+++ AniApi/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -22,12 +23,14 @@ namespace AniApi.Client
             string protocol = builder.Configuration["Protocol"];
             int port = int.Parse(builder.Configuration["Port"]);
             bool UseCustomVideoPlayer = bool.Parse(builder.Configuration["UseCustomVideoPlayer"]);
+            bool DarkTheme = bool.Parse(builder.Configuration["UseDarkTheme"]);
 
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); //Modificare se si vuole chiamare WebAPI
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(string.Format("{0}://{1}:{2}/", protocol, hostname, port)) });
 
             builder.Services.AddScoped<Generic>(); //Metodi Custom comuni a tutte le pagine
 
+            builder.Services.AddScoped<ThemeManager>(_ => new ThemeManager() { isDarkTheme = DarkTheme });
             builder.Services.AddSingleton<UIConfigurations>(_ => new UIConfigurations(UseCustomVideoPlayer));
 
             await builder.Build().RunAsync();
