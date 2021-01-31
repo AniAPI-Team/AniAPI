c66217adc085a47b05bba7c45e8eb304c2fcdf5d
Dazorn96 <marcodz554@gmail.com>
2021-01-31 13:40:39 +0100

    Implemented basic user creation

---
+++ Commons/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commons
+{
+    public class AppSettings : IModel
+    {
+        [BsonElement("jwt_secret")]
+        public string JWTSecret { get; set; }
+    }
+}
