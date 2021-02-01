6aaf6ab3349ae68a16b00625dae805320bf3dac8
Emerson Annoni <emerson.annoni@openjob.it>
2021-02-01 10:35:46 +0100

    added method infos in Generic class

--- WebApp/Generic.cs
+++ WebApp/Generic.cs
@@ -20,6 +20,16 @@ namespace WebApp
             this._httpClient = httpClient;
             this._spinner = spinner;
         }
+
+        /// <summary>
+        /// Post to Server Data and receive an Object with type defined in parameters
+        /// </summary>
+        /// <typeparam name="T">Data Type of server response</typeparam>
+        /// <typeparam name="Z">Data Type of server request</typeparam>
+        /// <param name="urlApi">Relative path to Server API</param>
+        /// <param name="data">Data to be sent to Server</param>
+        /// <param name="useSpinner">Activate the spinner while calling the server</param>
+        /// <returns>Object with type defined in parameters</returns>
         public async Task<T> PostSingleRequest<T, Z>(string urlApi, Z data, bool useSpinner = false) where T : new()
         {
             if (useSpinner)
@@ -57,6 +67,15 @@ namespace WebApp
             return res;
         }
 
+        /// <summary>
+        /// Post to Server Data and receive a List Object with type defined in parameters
+        /// </summary>
+        /// <typeparam name="T">Data Type of server response</typeparam>
+        /// <typeparam name="Z">Data Type of server request</typeparam>
+        /// <param name="urlApi">Relative path to Server API</param>
+        /// <param name="data">Data to be sent to Server</param>
+        /// <param name="useSpinner">Activate the spinner while calling the server</param>
+        /// <returns>List of Object with type defined in parameters</returns>
         public async Task<List<T>> PostListRequest<T, Z>(string urlApi, Z data, bool useSpinner = false) where T : class, new()
         {
             if (useSpinner)
@@ -94,6 +113,13 @@ namespace WebApp
             return res;
         }
 
+        /// <summary>
+        /// Get Data (Object with type defined in parameters) from Server
+        /// </summary>
+        /// <typeparam name="T">Data Type of server response</typeparam>
+        /// <param name="urlApi">Relative path to Server API</param>
+        /// <param name="useSpinner">Activate the spinner while calling the server</param>
+        /// <returns>Object with type defined in parameters</returns>
         public async Task<T> GetSingleRequest<T>(string urlApi, bool useSpinner = false) where T : class, new()
         {
             if (useSpinner)
@@ -128,6 +154,13 @@ namespace WebApp
             return res;
         }
 
+        /// <summary>
+        /// Get Data (List of Object with type defined in parameters) from Server
+        /// </summary>
+        /// <typeparam name="T">Data Type of server response</typeparam>
+        /// <param name="urlApi">Relative path to Server API</param>
+        /// <param name="useSpinner">Activate the spinner while calling the server</param>
+        /// <returns>List fo Object with type defined in parameters</returns>
         public async Task<List<T>> GetListRequest<T>(string urlApi, bool useSpinner = false) where T : class, new()
         {
             if (useSpinner)
