5dd461426f85acf57f81146ad2dd26626ad18218
MrTruth0 <gtatruth11943@gmail.com>
2021-06-02 02:19:45 +0200

    [AnimeWorldScraper]: Finished the code, implemented getEpisode

--- SyncService/Models/WebsiteScrapers/AnimeworldScraper.cs
+++ SyncService/Models/WebsiteScrapers/AnimeworldScraper.cs
@@ -3,6 +3,7 @@ using PuppeteerSharp;
 using SyncService.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,7 @@ namespace SyncService.Models.WebsiteScrapers
 
         protected override Type WebsiteType => typeof(AnimeworldScraper);
 
-        protected override async Task<EpisodeMatching> GetEpisode(Page webPage, AnimeMatching matching, int number)
-        {
-            throw new NotImplementedException();
-        }
+        private List<EpisodeMatching> episodesMatchings = new List<EpisodeMatching>();
 
         protected override async Task<AnimeMatching> GetMatching(Page webPage, string animeTitle)
         {
@@ -56,6 +54,94 @@ namespace SyncService.Models.WebsiteScrapers
                     this.Service.Log($"URL: { matching.Path }");
                     return matching;
                 }
+            }
+
+            return null;
+        }
+
+        protected override async Task<EpisodeMatching> GetEpisode(Page webPage, AnimeMatching matching, int number)
+        {
+            string url;
+
+            if (episodesMatchings.Count == 0)
+            {
+                url = this.Website.SiteUrl.Substring(0, this.Website.SiteUrl.Length - 1);
+                url = $"{url}{matching.Path}";
+                await webPage.GoToAsync(url);
+
+                await webPage.WaitForSelectorAsync(".widget.servers", new WaitForSelectorOptions()
+                {
+                    Visible = true,
+                    Timeout = 2000
+                });
+
+                // Description
+                ElementHandle description = await webPage.QuerySelectorAsync(".long");
+                matching.Description = (await description.EvaluateFunctionAsync<string>("e => e.innerHTML")).Trim();
+
+                // Get the Active Server (It will be always the Main server "AnimeWorld")
+                string serverID = await webPage.QuerySelectorAsync(".tab.server-tab.active")
+                    .EvaluateFunctionAsync<string>("e => e.getAttribute('data-name')");
+
+                foreach (ElementHandle server in await webPage.QuerySelectorAllAsync(".server"))
+                {
+                    string sID = await server.EvaluateFunctionAsync<string>("e => e.getAttribute('data-name')");
+                    if (serverID == sID)
+                    {
+                        foreach (ElementHandle ep in await server.QuerySelectorAllAsync("ul.episodes.range li.episode"))
+                        {
+                            ElementHandle info = await ep.QuerySelectorAsync("a");
+
+                            string path = await info.EvaluateFunctionAsync<string>("e => e.getAttribute('href')");
+                            string title = (await info.EvaluateFunctionAsync<string>("e => e.innerText")).Trim();
+
+                            episodesMatchings.Add(new EpisodeMatching()
+                            {
+                                Path = path,
+                                Title = title
+                            });
+                        }
+                        break;
+                    }
+                }
+            }
+
+            EpisodeMatching episode = episodesMatchings[number - 1];
+
+            if (episode != null)
+            {
+                episode.Number = number;
+
+                if (!string.IsNullOrEmpty(episode.Path))
+                {
+                    episode.Path = episode.Path.Trim();
+                    episode.Title = "";
+
+                    url = this.Website.SiteUrl.Substring(0, this.Website.SiteUrl.Length - 1);
+                    url = $"{url}{episode.Path}";
+                    var watch = Stopwatch.StartNew();
+
+                    await webPage.GoToAsync(url);
+
+                    watch.Stop();
+                    Console.WriteLine($"{episode.Path} - {number} - {watch.ElapsedMilliseconds}");
+
+                    await webPage.WaitForSelectorAsync("#download", new WaitForSelectorOptions()
+                    {
+                        Visible = true,
+                        Timeout = 2000
+                    });
+
+                    foreach (ElementHandle source in await webPage.QuerySelectorAllAsync("#download"))
+                    {
+                        ElementHandle tempSource = await source.QuerySelectorAsync("#downloadLink");
+                        episode.Source = await tempSource
+                            .EvaluateFunctionAsync<string>("e => e.getAttribute('href')");
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(episode.Source))
+                    return episode;
             }
 
             return null;
