2b3231e7f505bd0f7cd27e337527632c729569d7
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-04-22 12:24:13 -0700

    Add Animepisode scraper

--- ScraperEngine/resources/animepisode.py
+++ ScraperEngine/resources/animepisode.py
@@ -15,7 +15,7 @@ class AnimepisodeResource(ScraperResource):
 		# On this line, use the name you used inside MongoDB's websites collection
         super().__init__(app, "animepisode")
 
-    def fix_title(self, title: str) -> str:
+    def fix_title(self, title: str):
         # set the variable "is_dubbed" to true if the title contains "Dubbed" in it, otherwise set it to false, do this using regexp
         is_dubbed = title in "Dubbed"
         title = title.replace("Dubbed", "").replace("Subbed", "").replace("English", "").strip()
@@ -44,12 +44,12 @@ class AnimepisodeResource(ScraperResource):
         
     async def get_possible_matchings(self, res: falcon.Response, title: str) -> List[Matching]:
         matchings = []
-        url = f"{self.base_url}/search?q={title}"
+        url = f"{self.base_url}/?s={uri.encode(title)}"
         try:
             page = await execute_proxied_request(self, url)
             articles = page.select_one("#main").find_all("article")
             for article in articles:
-                content = article.find({ "class": "blog-entry-inner" }).find({"class": "blog-entry-content"})
+                content = article.find(class_="blog-entry-inner").find(class_="blog-entry-content")
                 link = content.find("header").find().find("a")
                 title = link.text
                 url = link.get("href")
@@ -62,14 +62,18 @@ class AnimepisodeResource(ScraperResource):
         return matchings
 
     async def get_episode(self, res: falcon.Response, path: str, number: int) -> List[Episode]:
-        episodes = []
+        episodes: List[Episode] = []
         url = f"{self.base_url}{path}"
         try:
             page = await execute_proxied_request(self, url)
-            video_container  = page.select_one("#anime-embed-container")
-            
+            embed_url = str(page.find("iframe").get("src"))
+            page = await execute_proxied_request(self, embed_url, {
+                "Referer": "https://animepisode.com/"
+            })
+            video = page.select_one("video")
+            video_url = video.select_one('source').get('src')
+            episodes.append(Episode(f"Episode {number}", video_url, video_url, format="mp4", quality=None))
 
-            
 
         except Exception as e:
             print(str(e))
