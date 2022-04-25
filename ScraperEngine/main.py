9b6b9912fbf5d9afb20583cb5829387237bc736e
Kapilarny <50770669+Kapilarny@users.noreply.github.com>
2022-04-25 20:57:51 +0200

    Remove other resources

--- ScraperEngine/main.py
+++ ScraperEngine/main.py
@@ -1,11 +1,9 @@
 import falcon
 import falcon.asgi
-from resources.animegg import AnimeggResource
 from resources.animeworld import AnimeworldResource
 
 from resources.dreamsub import DreamsubResource
 from resources.gogoanime import GogoanimeResource
-from resources.animepisode import AnimepisodeResource
 
 from resources.aniplaylist import AniplaylistResource
 
@@ -15,9 +13,6 @@ app = falcon.asgi.App()
 
 DreamsubResource(app)
 AnimeworldResource(app)
-AnimepisodeResource(app)
-AnimeggResource(app)
-
 GogoanimeResource(app)
 
 AniplaylistResource(app)
